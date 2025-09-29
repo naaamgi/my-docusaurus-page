17d76efba79cb204d6d0f9aba8eac0bac94e4943
naaamgi <99550574+naaamgi@users.noreply.github.com>
2025-09-29 20:51:21 +0900

    feat: Add GitHub Actions CI/CD workflow for Docusaurus deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,19 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://naaamgi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-docusaurus-page/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'naaamgi', // Usually your GitHub org/user name.
+  projectName: 'my-docusaurus-page', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
+
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
